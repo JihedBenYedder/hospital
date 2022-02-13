de40ccff077086a60807b6bacae754d33008dd07
Ben <ben@alpha.com>
2022-02-13 12:44:28 +0000

    fix

--- setup_k6.sh
+++ setup_k6.sh
@@ -2,5 +2,8 @@
 set -ex
 
 yum clean all
-
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#sudo yum install epel-release-latest-7.noarch.rpm -y
+#rpm --import /etc/pki/rpm-gpg/*GPG*
+sudo yum install https://dl.k6.io/rpm/repo.rpm  -y 
 sudo yum install k6  -y --nogpgcheck
