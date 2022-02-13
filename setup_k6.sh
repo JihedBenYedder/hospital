5800466989e4e8009313930390fc7c010d07c3f5
Ben <ben@alpha.com>
2022-02-13 12:10:05 +0000

    fix

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 rpm --import /etc/pki/rpm-gpg/*GPG*
 sudo yum install https://dl.k6.io/rpm/repo.rpm  -y
 sudo yum install k6  -y
