0da59fc3131002bdf6785887852752228f272501
Ben <ben@alpha.com>
2022-02-13 11:38:50 +0000

    fix

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ex
 
-sudo yum install wget -y
-sudo yum install https://dl.k6.io/rpm/repo.rpm -y  --nogpgcheck
+sudo yum install https://dl.k6.io/rpm/repo.rpm -y  
 sudo yum install k6 -y  --nogpgcheck
