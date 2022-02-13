7addbe6908310435cefa9bc9dda86adf0e6a08ae
Ben <ben@alpha.com>
2022-02-13 13:02:05 +0000

    fix

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -ex
 
-yum clean all
+sudo yum clean all
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 #sudo yum install epel-release-latest-7.noarch.rpm -y
 #rpm --import /etc/pki/rpm-gpg/*GPG*
-sudo yum reinstall  https://dl.k6.io/rpm/repo.rpm  -y 
-sudo yum reinstall  k6  -y --nogpgcheck
+sudo yum install  https://dl.k6.io/rpm/repo.rpm  -y 
+  if [ $? -ne 1 ]; then   # Exit on any any error except 'nothing to do' 
+      exit 0
+    fi
+sudo yum install  k6  -y --nogpgcheck
+  if [ $? -ne 1 ]; then   # Exit on any any error except 'nothing to do' 
+      exit 0
+    fi
