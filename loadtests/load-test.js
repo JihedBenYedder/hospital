154ee7da2a070cad69ea7528f37a9c3e63747d68
Ben <ben@alpha.com>
2022-02-14 12:59:32 +0000

    fix

--- loadtests/load-test.js
+++ loadtests/load-test.js
@@ -5,11 +5,11 @@ export const options = {
   duration: '1m',
   vus: 15,
   thresholds: {
-    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+    http_req_duration: ['p(95)<700'], // 95 percent of response times must be below 500ms
   },
 };
 
 export default function () {
-  http.get('http://k8s-dev-d86f3a475c-488485548.us-east-2.elb.amazonaws.com/hospital/beds/total');
+  http.get('http://k8s-dev-d86f3a475c-571980439.us-east-2.elb.amazonaws.com/hospital/beds/total');
   sleep(3);
 }
