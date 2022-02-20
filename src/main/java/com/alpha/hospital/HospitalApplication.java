a867c6c0e8d6ee39ce3c6c575d7133ee7bf69edc
Ben <ben@alpha.com>
2022-02-20 18:34:46 +0000

    fix

--- src/main/java/com/alpha/hospital/HospitalApplication.java
+++ src/main/java/com/alpha/hospital/HospitalApplication.java
@@ -1,15 +1,55 @@
 package com.alpha.hospital;
 
 import com.alpha.hospital.configuration.RedisProperties;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
 @SpringBootApplication(scanBasePackages =  {"com.alpha.hospital"})
 public class HospitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@KafkaListener(topics = "topic1")
+	public void listenWithHeaders(
+			@Payload String message,
+			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+		System.out.println(
+				"Received Message: " + message
+						+ "from partition: " + partition);
+	}
+
+	@Bean
+	public KafkaAdmin admin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		return new KafkaAdmin(configs);
 	}
 
 }
