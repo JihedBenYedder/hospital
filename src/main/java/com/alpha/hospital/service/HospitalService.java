270b0d72e05b6fe48635f6cba7109e9701ff5952
Ben <ben@alpha.com>
2022-02-22 17:09:46 +0000

    fixpo

--- src/main/java/com/alpha/hospital/service/HospitalService.java
+++ src/main/java/com/alpha/hospital/service/HospitalService.java
@@ -37,7 +37,7 @@ public class HospitalService {
     }
 
     public void handlePatient() {
-        System.out.println("handlPatient2");
+        System.out.println("handlPatient3");
         hospitalRepository.find("hospitalData").map(d ->  new HospitalData(d.getTotalBedsNumber(), d.getOccupiedBedsNumber()+1, 5)).flatMap(this::setHospitalData);
 
     }
