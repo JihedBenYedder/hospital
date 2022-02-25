b40b53384982fb6a00b63f85afd93ced4d1b1c95
Ben <ben@alpha.com>
2022-02-25 19:49:30 +0000

    fix

--- src/main/java/com/alpha/hospital/HospitalApplication.java
+++ src/main/java/com/alpha/hospital/HospitalApplication.java
@@ -1,28 +1,7 @@
 package com.alpha.hospital;
 
-import com.alpha.hospital.configuration.RedisProperties;
-import com.alpha.hospital.service.HospitalService;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.KafkaAdminClient;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication(scanBasePackages =  {"com.alpha.hospital"})
 public class HospitalApplication {
